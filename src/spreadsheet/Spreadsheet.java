84b5737a441f9806701772169e87a3d2eb240427
Roger Torra <elrogertorra@gmail.com>
2014-04-29 21:47:41 +0200

    Proyecto spreadsheet

--- src/spreadsheet/Spreadsheet.java
+++ src/spreadsheet/Spreadsheet.java
@@ -1,57 +1,29 @@
-package spreadsheet;
+package Spreadsheet;
 
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 /**
  *
- * @author Roger_Torra
+ * @author rtv1
  */
 public class Spreadsheet {
-
-
-	private int numRows, numCols;
-	private Celda[][] cells;
-
-
-
-	public Spreadsheet(int rows, int cols) {
-		numRows = rows;
-		numCols = cols;
-		cells = new Celda[numRows][numCols];
-		for(int i = 0; i < numRows; i++) {
-			for(int j = 0; j < numCols; j++) {
-				cells[i][j] = new Celda();
-			}
-		}
-	}
-
-	public Spreadsheet(int size) {
-		this(size, size);
-	}
-
-	public Spreadsheet() {
-		this(5);
-	}
-
-	public Celda getCelda(int row, int col) {
-		if (0 <= row && row < numRows && 0 <= col && col < numCols) {
-			return cells[row][col];
-		}
-		return null;
-	}
-
-	public String toString() {
-		String result = "";
-		for(int i = 0; i < numRows; i++) {
-			for(int j = 0; j < numCols; j++) {
-				result = result + cells[i][j].getValue() + '\t';
-			}
-			result += '\n';
-		}
-		return result;
-	}
-
+    
+    private static final Sheet SHEET = new Sheet(5);
+    
+    public static void put(String name,double value){
+        SHEET.setCell(name,value);
+    }
+     public static void put(String name){
+        SHEET.setCell(name);
+    }
+    public static MaybeValue get(String name){
+       return SHEET.getCell(name).getMvalue(); 
+    }
+    public static Expression plus(double a, double b){
+        
+    }
 }
